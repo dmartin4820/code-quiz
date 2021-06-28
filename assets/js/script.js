b67be794c45099d49fd038095c009256eb004f2c
Denzal Martin <dom4822@yahoo.com>
2021-06-27 17:54:50 -0700

    added timer and time deduction for wrong answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ var startButton = document.getElementById("start-button");
 var startButtonContainer = document.getElementById("card-start-container");
 var cardQuestion = document.getElementById("card-question");
 var cardQuestionHeader = document.getElementById("card-question-header");
+var timer = document.getElementById("timeleft");
 var answer1 = document.getElementById("answer1");
 var answer2 = document.getElementById("answer2");
 var answer3 = document.getElementById("answer3");
@@ -9,6 +10,7 @@ var answer4 = document.getElementById("answer4");
 var answers = [answer1, answer2, answer3, answer4];
 
 var questionIndex = 0;
+var timeLeft = 90;
 
 class question {
 	constructor(q,a,b) {
@@ -44,18 +46,39 @@ var question2 = new question(
 var questions = [question1, question2];
 
 function startQuiz() {
-	startButton.addEventListener("click", function() {
+	startButton.addEventListener("click", function(event) {
+		startTimer();
 		startButtonContainer.setAttribute("class", "card-start-container hide");
 		cardQuestion.setAttribute("class", "card-question show")
-		displayNextQuestion();
+		displayNextQuestion(event);
 	})
 }
+function startTimer() {
+	var interval = setInterval(function() {
+		timeLeft--;
+		timer.innerHTML = timeLeft + " seconds left";	
+		if (timeLeft === 0) {
+			clearInterval(interval);
+		}
+	}, 1000);
+}
 
-function displayNextQuestion() {
+function deductTime() {
+	timeLeft -= 10;
+}
+
+function displayNextQuestion(event) {
 	answer1.addEventListener("click", displayNextQuestion);
 	answer2.addEventListener("click", displayNextQuestion);
 	answer3.addEventListener("click", displayNextQuestion);
 	answer4.addEventListener("click", displayNextQuestion);
+
+	var selectedAnswerId = event.path[0].getAttribute("data-id");
+	if (questionIndex !== 0 && 
+		!questions[questionIndex - 1].answerBools[selectedAnswerId]) {
+			timeLeft -= 10;	
+	}
+	
 
 	cardQuestionHeader.innerHTML = questions[questionIndex].question;
 
